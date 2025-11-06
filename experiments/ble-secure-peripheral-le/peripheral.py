a0e466071205f208076c704e602fa1b66ec7fe8a
avU8989 <draagoon8888@live.de>
2025-11-06 12:41:44 +0100

    refactor(ble-peripheral-le): finally rebuild BLE Peripheral using custom implementation with dbus-next

--- experiments/ble-secure-peripheral-le/peripheral.py
+++ experiments/ble-secure-peripheral-le/peripheral.py
@@ -1,24 +1,42 @@
-import asyncio
-from asyncio import Queue, Event, create_task
-from bluez_peripheral.util import Adapter, get_message_bus, is_bluez_available
-from bluez_peripheral.advert import Advertisement
-from bluez_peripheral.gatt.service import Service, ServiceCollection
-from bluez_peripheral.gatt.characteristic import characteristic, CharacteristicFlags as CF
-from bluez_peripheral.agent import YesNoAgent  # for Numeric Comparison (MITM)
-from bluez_peripheral.advert import PacketType, AdvertisingIncludes
-from services.heart_rate_service import HeartRateService, HEARTRATE_SERVICE
-from services.physical_activtiy_service import PHYSICAL_ACTIVITY_SERVICE, PhysicalActivityMonitorService
-from services.pulse_oximeter_service import PulseOximeterService, PULSEOXIMETER_SERVICE
-from services.sleep_monitor_service import SleepMonitorService, SLEEP_MONITOR_SERVICE
-import logging
-from utils.adapter_utils import set_adapter_alias, set_adapter_discoverable, set_adapter_powered, find_adapter_props
-from utils.btmgmt_utils import setup_btmgmt
 
+from utils.adapter_utils import set_adapter_alias, cleanup, register_advert, register_gatt_application, register_agent
+from utils.btmgmt_utils import setup_btmgmt
+import dbus_next
+from dbus_next.aio import MessageBus
+import asyncio
+import traceback
+from asyncio import Queue, Event, create_task
+import logging
+from bluez_gatt.services.physical_activity_monitor_service import PhysicalActivityMonitorService
+from bluez_gatt.services.sleep_monitor_service import SleepMonitorService
+from bluez_gatt.services.pulse_oximeter_service import PulseOximeterService
+from bluez_gatt.services.heart_rate_service import HeartRateService
+from bluez_gatt.characteristics.heart_rate_meas_char import HeartMeasurementCharacteristic
+from bluez_gatt.characteristics.physical_activity_meas_char import StepCounterCharacteristic
+from bluez_gatt.characteristics.pulse_oximeter_meas_char import PulseOximeterMeasurementCharacteristic
+from bluez_gatt.characteristics.sleep_activity_meas_char import SleepMeasurementCharacteristic
+from bluez_gatt.advertisement import Advertisement
+from bluez_gatt.gatt_agent import Agent
+
+BLUEZ_SERVICE = "org.bluez"
+ADAPTER_PATH = "/org/bluez/hci1"
+AGENT_PATH = "/org/bluez/ble_secure_peripheral_le_sc_agent"
+
+# STANDARD UUIDS BY BLUETOOTH (SIG)
+HEARTRATE_SERVICE = "0000180d-0000-1000-8000-00805f9b34fb"
+HEARTRATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb"
+PULSEOXIMETER_SERVICE = "00001822-0000-1000-8000-00805f9b34fb"
+PLX_CONT_MEAS = "00002a5f-0000-1000-8000-00805f9b34fb"
+PHYSICAL_ACTIVITY_SERVICE = "0000183E-0000-1000-8000-00805f9b34fb"
+STEP_COUNTER_MEASUREMENT = "00002b40-0000-1000-8000-00805f9b34fb"
+SLEEP_MONITOR_SERVICE = "00001111-0000-1000-8000-00805f9b34fb"
+SLEEP_MEASUREMENT = "00002b41-0000-1000-8000-00805f9b34fb"
 
 DEVICE_NAME = "FitTrack"
 GREEN = "\033[92m"
 RESET = "\033[0m"
 YELLOW = "\033[93m"
+RED = "\033[91m"
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -82,8 +100,8 @@ async def stdin_reader_and_dispatch(pams_queue: Queue, sams_queue: Queue, stop_e
 
     while not stop_event.is_set():
         try:
-            # Attacker Peripheral Command (APCMD)
-            line = await loop.run_in_executor(None, input, f"{GREEN}[RPCMD]{RESET} ")
+            # Real Peripheral Command (RPMCD)
+            line = await loop.run_in_executor(None, input, f"{GREEN}[RPMCD]{RESET} ")
         except (EOFError, KeyboardInterrupt):
             print("Exiting...")
             stop_event.set()
@@ -140,103 +158,126 @@ async def stdin_reader_and_dispatch(pams_queue: Queue, sams_queue: Queue, stop_e
         if head == "sams":
             await sams_queue.put(rest)
             try:
-                await pams_queue.join()
+                await sams_queue.join()
             except asyncio.CancelledError:
                 break
 
         print("Unknown target. Prefix commands with 'pams' or 'sams'. Type 'help'.")
 
 
-async def on_request_confirmation(passkey: int) -> bool:
-    print(f"[AGENT] Confirm passkey: {passkey:06d} (auto-accepting)")
-    return True
-
-
-async def on_cancel():
-    print("[AGENT] Pairing was canceled by the peer")
-
-
 async def main():
-    if (setup_btmgmt() == False):
-        return
-
-    # by default message bus is set to system not to session
-    bus = await get_message_bus()
-
-    # for YesNoAgent
-    # YesNoAgent(request_confirmation: Callable[[int], Awaitable[bool]], cancel: Callable)
-    # we need an async function that receives the 6-digit passkey and returns TRUE or FALSE
-    # we need a function if pairing gets canceled
-    # guard against MITM attacks
-    agent = YesNoAgent(on_request_confirmation, on_cancel)
-
+    bus = await MessageBus(bus_type=dbus_next.BusType.SYSTEM).connect()
     try:
-        # register should be set on default in order to accept pairing requests
-        await agent.register(bus, default=True)
-    except Exception as e:
-        print(f"Failed to register agent: {e}")
+        node_info = await bus.introspect(BLUEZ_SERVICE, ADAPTER_PATH)
 
-        # create the services
+        # get the proxy object
+        proxy = bus.get_proxy_object(BLUEZ_SERVICE, ADAPTER_PATH, node_info)
 
-    heartrate_service = HeartRateService()
-    pulse_oximeter_service = PulseOximeterService()
-    physical_activity_service = PhysicalActivityMonitorService()
-    sleep_monitor_service = SleepMonitorService()
+        await set_adapter_alias(proxy, DEVICE_NAME)
 
-    try:
-        services = ServiceCollection()
-        services.add_service(heartrate_service)
-        services.add_service(pulse_oximeter_service)
-        services.add_service(physical_activity_service)
-        services.add_service(sleep_monitor_service)
-        await services.register(bus)
-    except Exception as e:
-        print(f"Failed to register serivce: {e}")
+        # create the agent
+        agent = Agent()
+        await register_agent(bus, agent, AGENT_PATH, capability="DisplayYesNo")
 
-    # set the alias could also be done on console with bluetoothctl
-    await set_adapter_alias(bus, DEVICE_NAME)
+        path = "/org/ble_secure_peripheral_le_sc"
+        heartrate_service_path = f"{path}/heartrate_service"
+        physical_activity_service_path = f"{path}/physical_activtiy_service"
+        pulse_oximeter_service_path = f"{path}/pulse_oximeter_service"
+        sleep_service_path = f"{path}/sleep_activity_service"
 
-    advert = Advertisement(
-        localName=DEVICE_NAME,
-        serviceUUIDs=[HEARTRATE_SERVICE,
-                      PULSEOXIMETER_SERVICE,
-                      PHYSICAL_ACTIVITY_SERVICE,
-                      SLEEP_MONITOR_SERVICE],
-        appearance=0,
-        timeout=0,
-        discoverable=True,
-        includes=AdvertisingIncludes.TX_POWER,
-        duration=65535
-    )
+        heartrate_meas_char_path = f"{heartrate_service_path}/heart_measurement"
+        step_counter_char_path = f"{physical_activity_service_path}/step_counter"
+        pulse_oximeter_meas_char_path = f"{pulse_oximeter_service_path}/pulse_oximeter_measurement"
+        sleep_meas_char_path = f"{sleep_service_path}/sleep_monitor_measurement"
 
-    try:
-        await advert.register(bus)
-        print(
-            f"{GREEN}-------------------Real Peripheral with LE started---------------")
+        # ------------------------Create GATT Characteristics-------------------------------
 
-        print(
-            f"-------------------Advertisment registered-----------------------{RESET}")
-    except Exception as e:
-        print(f"Failed to register advertisement: {e}")
+        # we are anticipating that the client pairs first in order to read our encrypted characteristics
+        # bluez checks link security level --> if not encrypted/authenticated start pairing process, pairing will therefore trigger agent
+        # to pair we need to add an agent from org.bluez.Agent with a passkey implementation
+        # after pairing suceeds the link becomes encrypted and authenticated
 
-    # create queues & stop event
-    pams_queue = Queue()  # Physical Activity Monitor Service
-    sams_queue = Queue()  # Sleep Activity Monitor Service
-    stop_event = Event()
+        heartrate_meas_char = HeartMeasurementCharacteristic(
+            heartrate_meas_char_path, HEARTRATE_MEASUREMENT, heartrate_service_path, ["encrypt-authenticated-read", "encrypt-authenticated-notify"])
 
-    tasks = [
-        asyncio.create_task(heartrate_service.start()),
-        asyncio.create_task(queue_control_consumer(
-            physical_activity_service, pams_queue, stop_event)),
-        asyncio.create_task(queue_control_consumer(
-            sleep_monitor_service, sams_queue, stop_event)),
-        asyncio.create_task(stdin_reader_and_dispatch(
-            pams_queue, sams_queue, stop_event))
-    ]
+        step_counter_char = StepCounterCharacteristic(
+            step_counter_char_path, STEP_COUNTER_MEASUREMENT, physical_activity_service_path, [
+                "encrypt-authenticated-read", "encrypt-authenticated-notify"]
+        )
+
+        pulse_oximeter_meas_char = PulseOximeterMeasurementCharacteristic(
+            pulse_oximeter_meas_char_path, PLX_CONT_MEAS, pulse_oximeter_service_path, ["encrypt-authenticated-read"])
+
+        sleep_meas_char = SleepMeasurementCharacteristic(
+            sleep_meas_char_path, SLEEP_MEASUREMENT, sleep_service_path, [
+                "encrypt-authenticated-read", "encrypt-authenticated-notify"]
+        )
+        # ------------------------Create GATT Services-------------------------------
+
+        heartrate_service = HeartRateService(
+            heartrate_service_path, HEARTRATE_SERVICE, heartrate_meas_char)
+
+        physical_activity_service = PhysicalActivityMonitorService(
+            physical_activity_service_path, PHYSICAL_ACTIVITY_SERVICE, step_counter_char
+        )
+
+        pulse_oximeter_service = PulseOximeterService(
+            pulse_oximeter_service_path, PULSEOXIMETER_SERVICE, pulse_oximeter_meas_char
+        )
+
+        sleep_activity_service = SleepMonitorService(
+            sleep_service_path, SLEEP_MONITOR_SERVICE, sleep_meas_char)
+
+        # ------------------------Export Service on Bluez System Bus------------
+
+        bus.export(heartrate_service_path, heartrate_service)
+        bus.export(physical_activity_service_path,
+                   physical_activity_service)
+        bus.export(pulse_oximeter_service_path,
+                   pulse_oximeter_service)
+        bus.export(sleep_service_path, sleep_activity_service)
+
+        # ------------------------Export Characteristics on Bluez System Bus------------
+
+        bus.export(heartrate_meas_char_path, heartrate_meas_char)
+        bus.export(step_counter_char_path, step_counter_char)
+        bus.export(pulse_oximeter_meas_char_path, pulse_oximeter_meas_char)
+        bus.export(sleep_meas_char_path, sleep_meas_char)
+
+        await register_gatt_application(proxy, path)
+
+        service_uuids = [HEARTRATE_SERVICE,
+                         PHYSICAL_ACTIVITY_SERVICE, PULSEOXIMETER_SERVICE, SLEEP_MONITOR_SERVICE]
+
+        advert_path = f"{path}/advert0"
+        advert = Advertisement(advert_path, DEVICE_NAME, service_uuids)
+
+        try:
+            await register_advert(bus, proxy, advert, advert_path)
+            print(
+                f"{GREEN}-------------------Real Peripheral with LE started---------------")
+
+            print(
+                f"-------------------Advertisment registered-----------------------{RESET}")
+        except Exception as e:
+            print(f"Failed to register advertisement: {e}")
+
+        # create queues & stop event
+        pams_queue = Queue()  # Physical Activity Monitor Service
+        sams_queue = Queue()  # Sleep Activity Monitor Service
+        stop_event = Event()
+
+        tasks = [asyncio.create_task(queue_control_consumer(
+            physical_activity_service, pams_queue, stop_event)), asyncio.create_task(queue_control_consumer(sleep_activity_service, sams_queue, stop_event))]
+
+    except Exception:
+        print("Top-level exception while setting up:")
+        traceback.print_exc()
 
     try:
         wait_task = [asyncio.create_task(
-            bus.wait_for_disconnect()), asyncio.create_task(stop_event.wait())]
+            bus.wait_for_disconnect()), asyncio.create_task(stop_event.wait()), asyncio.create_task(stdin_reader_and_dispatch(
+                pams_queue, sams_queue, stop_event))]
 
         # clean shutdown, we wait until BlueZ disconnects or stop_event is set
         done, pending = await asyncio.wait(wait_task, return_when=asyncio.FIRST_COMPLETED)
@@ -245,6 +286,7 @@ async def main():
             t.cancel()
     finally:
         stop_event.set()
+        await cleanup(bus, proxy, advert_path, path)
 
         for t in tasks:
             t.cancel()
